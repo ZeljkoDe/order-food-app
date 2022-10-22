5b870ada84dbc581fd757c21224dcdf2be26e420
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-10-22 18:25:04 +0200

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from 'components/Layout/Header';
 import './App.css';
 
 function App() {
-	return <div>React</div>;
+	return (
+		<>
+			<Header />
+		</>
+	);
 }
 
 export default App;
