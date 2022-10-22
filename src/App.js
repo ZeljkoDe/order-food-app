379fc7f6c0f415c0c9c6918b32d51e497dab084d
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-10-22 18:48:12 +0200

    Meals Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from 'components/Layout/Header';
+import Meals from 'components/Meals';
 import './App.css';
 
 function App() {
 	return (
 		<>
 			<Header />
+			<main>
+				<Meals />
+			</main>
 		</>
 	);
 }
