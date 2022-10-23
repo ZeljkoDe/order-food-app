fb3d215fe03556d5e1e461139b3303f7a3ee6634
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-10-23 07:35:54 +0200

    Cart Logic - add to cart

--- src/components/Meals/MealItem/index.jsx
+++ src/components/Meals/MealItem/index.jsx
@@ -1,8 +1,21 @@
+import { useContext } from 'react';
+import CartContext from 'store/cart-content';
 import MealItemForm from '../MealItemForm';
 import classes from './style.module.css';
 
 const MealItem = ({ name, description, price, id }) => {
+	const cartCtx = useContext(CartContext);
 	const priceFormatted = `$${price.toFixed(2)}`;
+
+	const addToCartHandler = (amount) => {
+		cartCtx.addItem({
+			id: id,
+			name: name,
+			amount: amount,
+			price: price,
+		});
+	};
+
 	return (
 		<li className={classes.meal}>
 			<div>
@@ -11,7 +24,7 @@ const MealItem = ({ name, description, price, id }) => {
 				<div className={classes.price}>{priceFormatted}</div>
 			</div>
 			<div>
-				<MealItemForm id={id} />
+				<MealItemForm id={id} onAddToCart={addToCartHandler} />
 			</div>
 		</li>
 	);
