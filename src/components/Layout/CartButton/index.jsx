c72aa12f4498e0500729ddd047863f11589bd733
Zeljko Damnjanovic <zeljko.de@outlook.com>
2022-10-27 07:33:32 +0200

    Class manipulation with useEffect

--- src/components/Layout/CartButton/index.jsx
+++ src/components/Layout/CartButton/index.jsx
@@ -1,17 +1,36 @@
 import CartIcon from 'components/Cart/CartIcon';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from 'store/cart-content';
 import classes from './style.module.css';
 
 const CartButton = ({ onClick }) => {
+	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 	const cartCtx = useContext(CartContext);
+	const { items } = cartCtx;
 
-	const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+	const numberOfCartItems = items.reduce((curNumber, item) => {
 		return curNumber + item.amount;
 	}, 0);
 
+	const btnClasses = `${classes.button} ${btnIsHighlighted && classes.bump}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+		setBtnIsHighlighted(true);
+
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button onClick={onClick} className={classes.button}>
+		<button onClick={onClick} className={btnClasses}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
